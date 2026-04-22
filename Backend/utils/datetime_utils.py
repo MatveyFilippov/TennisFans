8d0f049466522cfa205d44de4b7b274e6c0eddc3
MatveyFilippov <matdufi2004@gmail.com>
2026-04-22 18:46:57 +0300

    backend: add logging in utils

--- Backend/utils/datetime_utils.py
+++ Backend/utils/datetime_utils.py
@@ -3,17 +3,21 @@ from datetime import datetime, tzinfo, timezone
 from functools import lru_cache
 
 
+log = settings.ProjectLoggerFactory.get_for("utils.datetime")
+
+
+@lru_cache(maxsize=10_000)
 def datetime_to_timezone(dt: datetime, tz: tzinfo):
     if dt.tzinfo == tz:
         return dt
     return dt.replace(tzinfo=tz) if dt.tzinfo is None else dt.astimezone(tz)
 
 
-@lru_cache(maxsize=10_000)
 def utc_datetime(dt: datetime) -> datetime:
+    log.debug(f"Setting {dt.isoformat()} to utc")
     return datetime_to_timezone(dt=dt, tz=timezone.utc)
 
 
-@lru_cache(maxsize=10_000)
 def localize_datetime(dt: datetime) -> datetime:
+    log.debug(f"Localize {dt.isoformat()}")
     return datetime_to_timezone(dt=dt, tz=settings.PROJECT_TIMEZONE)
--- Backend/utils/players_pair_utils.py
+++ Backend/utils/players_pair_utils.py
@@ -5,6 +5,9 @@ from functools import lru_cache
 import networkx as nx
 
 
+log = settings.ProjectLoggerFactory.get_for("utils.playerspairs")
+
+
 @lru_cache(maxsize=10_000)
 def __calculate_players_pair_weight(last_played: datetime | None, current_time: datetime) -> float:
     if last_played is None:
@@ -13,6 +16,7 @@ def __calculate_players_pair_weight(last_played: datetime | None, current_time: 
 
 
 def find_optimal_players_pairs(all_players_id_dto: dict[int, dto.PlayerDTO], players_pair_dto_last_play: dict[dto.PlayersPairDTO, datetime]) -> list[dto.PlayersPairDTO]:
+    log.debug("Start computing optimal PlayersPairs")
     current_time = datetime.now(tz=settings.PROJECT_TIMEZONE)
     all_player_ids = list(sorted(all_players_id_dto.keys()))
 
@@ -26,10 +30,13 @@ def find_optimal_players_pairs(all_players_id_dto: dict[int, dto.PlayerDTO], pla
 
     for i, player1_id in enumerate(all_player_ids):
         for player2_id in all_player_ids[i + 1:]:
+            log.debug(f"Calculating PlayersPair[player1_id={player1_id};player2_id={player2_id}] weight")
             last_played = player_ids_pair_last_play.get((player1_id, player2_id))
             weight = __calculate_players_pair_weight(last_played, current_time)
             weighted_graph.add_edge(player1_id, player2_id, weight=weight)
+            log.debug(f"PlayersPair[player1_id={player1_id};player2_id={player2_id}] weight={weight}")
 
+    log.debug("Looking for Players max weight matching (with max cardinality)")
     matching = nx.max_weight_matching(weighted_graph, weight="weight", maxcardinality=True)
     result = []
     for player_ids_pair in matching:
@@ -38,4 +45,5 @@ def find_optimal_players_pairs(all_players_id_dto: dict[int, dto.PlayerDTO], pla
             player1_dto=all_players_id_dto[player1_id],
             player2_dto=all_players_id_dto[player2_id],
         ))
+    log.debug("End computing optimal PlayersPairs")
     return result
